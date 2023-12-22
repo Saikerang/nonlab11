82be57d5c7c7553fda54a333773201af2d343aee
Saikerang <Gaymaster@non.com>
2023-12-22 14:41:04 +0700

    Completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,22 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    
+    srand(time(0));
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    cout << "Press Enter 3 times to reveal your future.";
+
+
+    int x = 0;
+    while (x < 3) {
+    cin.get();
+    x++;
+    }
+
+    int random = rand() % 9;
+    string predictedGrade = grades[random];
+    cout << "You will get "<<predictedGrade<<" in this 261102.\n";
+}
